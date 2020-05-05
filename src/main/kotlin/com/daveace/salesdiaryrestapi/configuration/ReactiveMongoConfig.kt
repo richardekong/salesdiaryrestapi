9fa723bc8b517c4123f0b7e8169b197a04c4fe5a
richard <richardekong290@gmail.com>
2020-05-05 23:00:14 +0100

    create mongodb configuration

---
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/configuration/ReactiveMongoConfig.kt
@@ -0,0 +1,32 @@
+package com.daveace.salesdiaryrestapi.config
+
+import com.mongodb.reactivestreams.client.MongoClient
+import com.mongodb.reactivestreams.client.MongoClients
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration
+import org.springframework.data.mongodb.core.ReactiveMongoTemplate
+import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories
+
+@Configuration
+@EnableReactiveMongoRepositories(basePackages = ["com.daveace.salesdiaryrestapi.repository"])
+class ReactiveMongoConfig : AbstractReactiveMongoConfiguration() {
+
+    @Value("\${spring.data.mongodb.database}")
+    lateinit var dbName:String
+
+    override fun reactiveMongoClient(): MongoClient {
+        return MongoClients.create()
+    }
+
+    override fun getDatabaseName(): String {
+        return dbName
+    }
+
+    @Bean
+    fun salesDiaryReactiveMongoTemplate():ReactiveMongoTemplate{
+        return ReactiveMongoTemplate(reactiveMongoClient(), databaseName)
+    }
+
+}
