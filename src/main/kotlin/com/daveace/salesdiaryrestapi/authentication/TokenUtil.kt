1d215d456d63dcec6ad60fecd74493c2ed3e4fb7
richard <richardekong290@gmail.com>
2020-05-06 21:59:45 +0100

    fix bug

--- src/main/kotlin/com/daveace/salesdiaryrestapi/authentication/TokenUtil.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/authentication/TokenUtil.kt
@@ -13,11 +13,9 @@ import java.util.*
 @PropertySource("classpath:jwt.properties")
 class TokenUtil {
 
-    @Value("\${jwt.secret}")
-    private lateinit var secret:String
-
     companion object {
         const val TOKEN_VALIDITY = 1800000L
+        const val SECRET = "\${jwt.secret}"
     }
 
     fun getEmailFromToken(token: String): String {
@@ -28,7 +26,7 @@ class TokenUtil {
         return getAllClaimsFromToken(token).expiration
     }
 
-    fun generateToken(user: User, validity:Long = TOKEN_VALIDITY): String {
+    fun generateToken(user: User, validity: Long = TOKEN_VALIDITY): String {
         val claims: MutableMap<String, Any> = mutableMapOf()
         return doGenerateToken(claims, user.email, validity)
     }
@@ -36,7 +34,7 @@ class TokenUtil {
     fun getAllClaimsFromToken(token: String): Claims {
         return Jwts
                 .parser()
-                .setSigningKey(secret)
+                .setSigningKey(SECRET)
                 .parseClaimsJws(token)
                 .body
     }
@@ -47,18 +45,18 @@ class TokenUtil {
                 .setSubject(subject)
                 .setIssuedAt(Date(System.currentTimeMillis()))
                 .setExpiration(Date(System.currentTimeMillis() + validity))
-                .signWith(SignatureAlgorithm.HS256, secret)
+                .signWith(SignatureAlgorithm.HS256, SECRET)
                 .compact()
     }
 
-    fun isTokenExpired(token:String):Boolean{
-        val expiration:Date= getExpirationDateFromToken(token)
+    fun isTokenExpired(token: String): Boolean {
+        val expiration: Date = getExpirationDateFromToken(token)
         return expiration.before(Date())
     }
 
-    fun validateToken(token:String, usr:User):Boolean{
-        val email:String = getEmailFromToken(token)
-        return email == usr.email &&  !isTokenExpired(token)
+    fun validateToken(token: String, usr: User): Boolean {
+        val email: String = getEmailFromToken(token)
+        return email == usr.email && !isTokenExpired(token)
     }
 
 }
