a415594f60f5a990eb462617837f5c2d23f84412
richard <richardekong290@gmail.com>
2020-05-11 10:30:22 +0100

    create InMemoryTokenStore.kt to temporarily store and manage tokens while the user is logged into the system

---
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/repository/InMemoryTokenStore.kt
@@ -0,0 +1,28 @@
+package com.daveace.salesdiaryrestapi.repository
+
+import com.daveace.salesdiaryrestapi.authentication.TokenUtil
+
+class InMemoryTokenStore {
+    companion object {
+
+        private val tokenStore: MutableMap<String, String> = mutableMapOf()
+        private val tokenUtil: TokenUtil = TokenUtil()
+
+        fun storeToken(token: String) {
+            val email: String = tokenUtil.getEmailFromToken(token)
+            tokenStore.putIfAbsent(email, token)
+        }
+
+        fun findToken(email: String): String? {
+            return tokenStore[email]
+        }
+
+        fun revokeToken(email: String) {
+            tokenStore.remove(email)
+        }
+
+        fun isRevoked(token: String): Boolean {
+            return findToken(tokenUtil.getEmailFromToken(token)) != token
+        }
+    }
+}
