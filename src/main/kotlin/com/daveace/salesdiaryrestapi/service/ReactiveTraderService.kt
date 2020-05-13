9a3da28a9f6f6f6c65fb8ead57aec5cd991724a1
richard <richardekong290@gmail.com>
2020-05-13 23:20:44 +0100

    declared methods required to perform crud operarions on Trader collection

--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveTraderService.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveTraderService.kt
@@ -1,4 +1,12 @@
 package com.daveace.salesdiaryrestapi.service
 
+import com.daveace.salesdiaryrestapi.domain.Trader
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+
 interface ReactiveTraderService {
+    fun create(trader:Trader): Mono<Trader>
+    fun findTrader(email:String):Mono<Trader>
+    fun findAllTraders(): Flux<Trader>
+    fun updateTrader(email:String):Mono<Trader>
 }
