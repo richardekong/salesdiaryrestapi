bed1c94d08c52d8acd278cc617ef007176489252
richard <richardekong290@gmail.com>
2020-05-13 23:29:37 +0100

    remove updateTrader() and createTrader() functions

--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveTraderService.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveTraderService.kt
@@ -5,8 +5,6 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface ReactiveTraderService {
-    fun create(trader:Trader): Mono<Trader>
     fun findTrader(email:String):Mono<Trader>
     fun findAllTraders(): Flux<Trader>
-    fun updateTrader(email:String):Mono<Trader>
 }
--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveTraderServiceImpl.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveTraderServiceImpl.kt
@@ -1,8 +1,11 @@
 package com.daveace.salesdiaryrestapi.service
 
+import com.daveace.salesdiaryrestapi.domain.Trader
 import com.daveace.salesdiaryrestapi.repository.ReactiveTraderRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 
 @Service
 class ReactiveTraderServiceImpl : ReactiveTraderService{
@@ -10,5 +13,20 @@ class ReactiveTraderServiceImpl : ReactiveTraderService{
     @Autowired
     private lateinit var traderRepo:ReactiveTraderRepository
 
+    override fun create(trader: Trader): Mono<Trader> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findTrader(email: String): Mono<Trader> {
+        TODO("Not yet implemented")
+    }
+
+    override fun findAllTraders(): Flux<Trader> {
+        TODO("Not yet implemented")
+    }
+
+    override fun updateTrader(email: String): Mono<Trader> {
+        TODO("Not yet implemented")
+    }
 }
 
