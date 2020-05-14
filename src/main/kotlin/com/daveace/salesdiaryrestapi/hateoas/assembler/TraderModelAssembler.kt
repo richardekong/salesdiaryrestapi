1c759f103ae121e36b6f9821777903483518f6c1
richard <richardekong290@gmail.com>
2020-05-14 11:24:17 +0100

    refactor toModel() function in TraderModelAssembler.kt

--- src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/assembler/TraderModelAssembler.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/assembler/TraderModelAssembler.kt
@@ -1,16 +1,23 @@
 package com.daveace.salesdiaryrestapi.hateoas.assembler
 
 import com.daveace.salesdiaryrestapi.controller.TraderController
+import com.daveace.salesdiaryrestapi.controller.UserController
 import com.daveace.salesdiaryrestapi.domain.Trader
 import com.daveace.salesdiaryrestapi.hateoas.model.TraderModel
 import org.springframework.hateoas.CollectionModel
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo
 
 class TraderModelAssembler : RepresentationModelAssemblerSupport<Trader, TraderModel>
 (TraderController::class.java, TraderModel::class.java) {
 
-    override fun toModel(entity: Trader): TraderModel {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun toModel(trader: Trader): TraderModel {
+        return instantiateModel(trader)
+                .add(linkTo(methodOn(UserController::class.java)
+                        .findUserByEmail(trader.email))
+                        .withSelfRel()
+                        .toMono().toFuture().join())
     }
 
     override fun toCollectionModel(traders: MutableIterable<Trader>): CollectionModel<TraderModel> {
