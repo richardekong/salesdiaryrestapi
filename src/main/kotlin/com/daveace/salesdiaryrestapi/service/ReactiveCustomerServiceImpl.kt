884f9adf997af7a7883561771d3dbb509f668943
richard <richardekong290@gmail.com>
2020-06-07 21:56:31 +0100

    implement methods from ReactiveCustomerService.kt

--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveCustomerServiceImpl.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveCustomerServiceImpl.kt
@@ -1,5 +1,6 @@
 package com.daveace.salesdiaryrestapi.service
 
+import com.daveace.salesdiaryrestapi.authentication.AuthenticatedUser
 import com.daveace.salesdiaryrestapi.domain.Customer
 import com.daveace.salesdiaryrestapi.exceptionhandling.RestException
 import com.daveace.salesdiaryrestapi.repository.ReactiveCustomerRepository
@@ -16,19 +17,67 @@ class ReactiveCustomerServiceImpl() : ReactiveCustomerService {
     private lateinit var customerRepo: ReactiveCustomerRepository
 
     @Autowired
-    constructor(customerRepo: ReactiveCustomerRepository):this(){
+    private lateinit var authenticatedUser: AuthenticatedUser
+
+    @Autowired
+    constructor(customerRepo: ReactiveCustomerRepository) : this() {
         this.customerRepo = customerRepo
     }
 
-    override fun findCustomerByEmail(email: String): Mono<Customer> {
-        return customerRepo.findById(email)
+    override fun saveIfAbsent(customer: Customer): Mono<Customer> {
+        val email: String = customer.email
+        return customerRepo.existsCustomerByEmail(email)
                 .subscribeOn(Schedulers.parallel())
-                .switchIfEmpty(Mono.fromRunnable {
-                    throw RestException(HttpStatus.NOT_FOUND.reasonPhrase)
-                })
+                .filter { customerExists ->
+                    if (customerExists) throw RuntimeException(
+                            "Customer with $email exists!")
+                    else customerExists.not()
+                }
+                .flatMap { customerRepo.save(customer) }
+    }
+
+    override fun findCustomerById(id: String): Mono<Customer> {
+        return authenticatedUser.getCurrentUser()
+                .flatMap { currentUser ->
+                    customerRepo.findById(id)
+                            .subscribeOn(Schedulers.parallel())
+                            .filter { customer ->
+                                customer.traderId == currentUser.id
+                            }
+                            .switchIfEmpty(Mono.fromRunnable {
+                                throw RestException(HttpStatus.UNAUTHORIZED.reasonPhrase)
+                            })
+                }
+    }
+
+    override fun findCustomerByEmail(email: String): Mono<Customer> {
+
+        return authenticatedUser.getCurrentUser()
+                .flatMap { currentUser ->
+                    customerRepo.findCustomerByEmail(email)
+                            .subscribeOn(Schedulers.parallel())
+                            .filter { customer -> customer.traderId == currentUser.id }
+                            .switchIfEmpty(Mono.fromRunnable {
+                                throw RestException(HttpStatus.UNAUTHORIZED.reasonPhrase)
+                            })
+                }
     }
 
     override fun findAllCustomers(): Flux<Customer> {
-        return customerRepo.findAll()
+        return authenticatedUser.getCurrentUser()
+                .flatMapMany { currentUser ->
+                    customerRepo.findAll()
+                            .subscribeOn(Schedulers.parallel())
+                            .filter { customer ->
+                                customer.traderId == currentUser.id
+                            }
+                            .switchIfEmpty(Mono.fromRunnable {
+                                throw RestException(HttpStatus.UNAUTHORIZED.reasonPhrase)
+                            })
+                }
+    }
+
+    override fun existsByEmail(email: String): Mono<Boolean> {
+        return customerRepo.existsCustomerByEmail(email)
     }
 }
