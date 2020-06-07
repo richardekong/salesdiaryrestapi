962913f9b16bbc18c6c413252398dabb525c59e4
richard <richardekong290@gmail.com>
2020-06-07 21:48:03 +0100

    add functions: findCustomerByEmail(email:String) and existsCustomerByEmail(email:String)

--- src/main/kotlin/com/daveace/salesdiaryrestapi/repository/ReactiveCustomerRepository.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/repository/ReactiveCustomerRepository.kt
@@ -1,7 +1,15 @@
 package com.daveace.salesdiaryrestapi.repository
 
 import com.daveace.salesdiaryrestapi.domain.Customer
+import com.daveace.salesdiaryrestapi.exceptionhandling.RestException
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 
-interface ReactiveCustomerRepository: ReactiveMongoRepository<Customer, String> {
+interface ReactiveCustomerRepository : ReactiveMongoRepository<Customer, String> {
+    fun findCustomerByEmail(email: String): Mono<Customer>
+
+    fun existsCustomerByEmail(email: String):Mono<Boolean>{
+        return findCustomerByEmail(email).map { it != null }
+    }
 }
