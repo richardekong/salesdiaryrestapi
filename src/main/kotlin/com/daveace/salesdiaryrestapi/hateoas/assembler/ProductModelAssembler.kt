fa304f64d932d34ff94d93e3368ee4fe39b5b9ac
richard <richardekong290@gmail.com>
2020-06-07 21:40:58 +0100

    refactor toModel(p:product) function

--- src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/assembler/ProductModelAssembler.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/assembler/ProductModelAssembler.kt
@@ -5,13 +5,19 @@ import com.daveace.salesdiaryrestapi.domain.Product
 import com.daveace.salesdiaryrestapi.hateoas.model.ProductModel
 import org.springframework.hateoas.CollectionModel
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn
 
 class ProductModelAssembler : RepresentationModelAssemblerSupport<Product, ProductModel>(
         ProductController::class.java,
         ProductModel::class.java
 ) {
     override fun toModel(product: Product): ProductModel {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return instantiateModel(product)
+                .add(linkTo(methodOn(ProductController::class.java)
+                        .findProduct(product.id))
+                        .withSelfRel()
+                        .toMono().toFuture().join())
     }
 
     override fun toCollectionModel(product: MutableIterable<Product>): CollectionModel<ProductModel> {
