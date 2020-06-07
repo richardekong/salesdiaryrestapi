846d7f61901552a3b0cef7a7e03bee6bcee69701
richard <richardekong290@gmail.com>
2020-06-07 21:57:08 +0100

    add function declarations

--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveProductService.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveProductService.kt
@@ -1,5 +1,12 @@
 package com.daveace.salesdiaryrestapi.service
 
+import com.daveace.salesdiaryrestapi.domain.Product
+import reactor.core.publisher.Mono
+
 
 interface ReactiveProductService {
+
+    fun saveIfAbsent(product: Product):Mono<Product>
+    fun findProduct(id:String): Mono<Product>
+    fun existsByName(name:String):Mono<Boolean>
 }
