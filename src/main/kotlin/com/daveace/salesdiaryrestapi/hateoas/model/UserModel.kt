50e61fa47fe034aef77a7658d9907da9873fe395
richard <richardekong290@gmail.com>
2020-06-07 21:44:39 +0100

    refactor secondary constructor

--- src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/UserModel.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/UserModel.kt
@@ -1,7 +1,6 @@
 package com.daveace.salesdiaryrestapi.hateoas.model
 
 import com.daveace.salesdiaryrestapi.domain.User
-import com.fasterxml.jackson.annotation.JsonIgnore
 import org.springframework.hateoas.RepresentationModel
 import org.springframework.hateoas.server.core.Relation
 
@@ -10,14 +9,11 @@ class UserModel() : RepresentationModel<UserModel>() {
 
     lateinit var email: String
     lateinit var password: String
-    lateinit var phone: String
 
     constructor(user:User):this(){
         this.email = user.email
         this.password = user.password
-        this.phone = user.phone
     }
-
 
 }
 
