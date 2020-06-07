338c587bdd6cecf087ce16dc16ee6df670ffed80
richard <richardekong290@gmail.com>
2020-06-07 21:51:47 +0100

    add declaration for findUserByEmail(email:String) and implementation for existsByEmail(email:String)

--- src/main/kotlin/com/daveace/salesdiaryrestapi/repository/ReactiveUserRepository.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/repository/ReactiveUserRepository.kt
@@ -2,7 +2,15 @@ package com.daveace.salesdiaryrestapi.repository
 
 import com.daveace.salesdiaryrestapi.domain.User
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Mono
 
 
-interface ReactiveUserRepository: ReactiveMongoRepository<User, String> {
+interface ReactiveUserRepository : ReactiveMongoRepository<User, String> {
+
+    fun findUserByEmail(email: String): Mono<User>
+
+    fun existsByEmail(email: String): Mono<Boolean> {
+        return findUserByEmail(email).map { it != null }
+    }
+
 }
