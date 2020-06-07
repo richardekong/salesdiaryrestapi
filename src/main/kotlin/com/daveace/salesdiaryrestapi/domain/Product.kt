9703b5be83a055e19085c7b80dfbdf5165968c80
richard <richardekong290@gmail.com>
2020-06-07 21:36:08 +0100

    modify both primary and secondary constructors

--- src/main/kotlin/com/daveace/salesdiaryrestapi/domain/Product.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/domain/Product.kt
@@ -7,28 +7,37 @@ import javax.validation.constraints.*
 
 @Document
 data class Product(
+        @Id
+        val id: String = UUID.randomUUID().toString(),
+        var traderId: String = "",
         @field:NotBlank(message = PRODUCT_NAME_VAL_MSG)
         @field:Size(min = PRODUCT_NAME_SIZE, message = PRODUCT_NAME_SIZE_VAL_MSG)
-        var name: String
-) {
+        var name: String = "",
+        @field:NotBlank(message = PRODUCT_CODE_VAL_MSG)
+        var code: String = "",
+        var imagePath: String = "",
+        val date: Date = Date()) {
 
-    @Id
-    val id: String = UUID.randomUUID().toString()
-    lateinit var traderId: String
-    val date: Date = Date()
-    @field:NotNull(message = PRODUCT_CODE_VAL_MSG)
-    lateinit var code: String
-    lateinit var imagePath: String
     @field:DecimalMin("0.0")
     var stock: Double = 0.0
         set(value) {
-            if (value >= 0) field = value
+            if (value >= 0.0) field = value
         }
+
     @field:DecimalMin("1.0")
     var cost: Double = 0.0
         set(value) {
-            if (value >= 0) field = value
+            if (value >= 0.0) field = value
         }
+
+    constructor(traderId: String, name: String, code: String, imagePath: String, stock: Double, cost: Double) : this() {
+        this.traderId = traderId
+        this.name = name
+        this.code = code
+        this.imagePath = imagePath
+        this.stock = stock
+        this.cost = cost
+    }
 
 }
 
