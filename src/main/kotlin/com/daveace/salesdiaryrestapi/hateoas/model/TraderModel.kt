91fe07d4ab06a43946d1ed2ee3e96c54d05a255b
richard <richardekong290@gmail.com>
2020-06-07 21:44:12 +0100

    refactor secondary constructor

--- src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/TraderModel.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/TraderModel.kt
@@ -9,15 +9,17 @@ import org.springframework.hateoas.server.core.Relation
 @Relation(value = "trader", collectionRelation = "traders")
 class TraderModel() : RepresentationModel<TraderModel>() {
 
+    lateinit var id: String
     lateinit var email: String
     lateinit var name: String
-    lateinit var phone:String
+    lateinit var phone: String
     lateinit var address: String
-    lateinit var customers:MutableList<Customer>
-    lateinit var products:MutableList<Product>
+    lateinit var customers: MutableList<Customer>
+    lateinit var products: MutableList<Product>
     lateinit var location: MutableList<Double>
 
     constructor(trader: Trader) : this() {
+        this.id = trader.id
         this.email = trader.email
         this.name = trader.name
         this.phone = trader.phone
