e18aa7301be454c62ee1811d7c465455db1bc553
richard <richardekong290@gmail.com>
2020-06-07 21:54:50 +0100

    add 3 more function declaration

--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveCustomerService.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveCustomerService.kt
@@ -5,7 +5,10 @@ import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 interface ReactiveCustomerService {
+    fun saveIfAbsent(customer:Customer):Mono<Customer>
+    fun findCustomerById(id:String):Mono<Customer>
     fun findCustomerByEmail(email:String): Mono<Customer>
     fun findAllCustomers():Flux<Customer>
+    fun existsByEmail(email: String):Mono<Boolean>
 }
 
