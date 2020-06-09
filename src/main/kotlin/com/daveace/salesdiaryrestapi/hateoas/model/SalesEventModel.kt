32339a7577b401a722f233804b5bbfaae26410fa
richard <richardekong290@gmail.com>
2020-06-09 17:48:27 +0100

    refactor this class

--- src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/SalesEventModel.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/SalesEventModel.kt
@@ -12,18 +12,20 @@ class SalesEventModel() : RepresentationModel<SalesEventModel>() {
     lateinit var id: String
     lateinit var traderId: String
     lateinit var productId: String
-    var price: Double = 0.0
-    var sales: Double = 0.0
+    var salesPrice: Double = 0.0
+    var quantitySold: Double = 0.0
+    var costPrice:Double = 0.0
     var left: Double = 0.0
     lateinit var date: Date
-    lateinit var location: Point
+    lateinit var location: MutableList<Double>
 
     constructor(event: SalesEvent) : this() {
         this.id = event.id
         this.traderId = event.traderId
         this.productId = event.productId
-        this.price = event.price
-        this.sales = event.sales
+        this.costPrice = event.costPrice
+        this.salesPrice = event.salesPrice
+        this.quantitySold = event.quantitySold
         this.left = event.left
         this.date = event.date
         this.location = event.location
