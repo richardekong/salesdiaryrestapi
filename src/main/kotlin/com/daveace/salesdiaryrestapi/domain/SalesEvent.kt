a6ce96d693155490dad2e233e1a85618a334fba2
richard <richardekong290@gmail.com>
2020-06-09 12:38:44 +0100

    refactored constructor and rename price, cost and sales properties to salesPrice, costPrice and quantitySold respectively

--- src/main/kotlin/com/daveace/salesdiaryrestapi/domain/SalesEvent.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/domain/SalesEvent.kt
@@ -11,19 +11,19 @@ import javax.validation.constraints.NotNull
 
 @Document
 data class SalesEvent(
+        @Id
+        val id: String = UUID.randomUUID().toString(),
         var traderId: String,
         var productId: String,
-        var customerId:String,
+        var customerId: String,
         @field:DecimalMin(value = "0.00")
-        val sales: Double,
+        val quantitySold: Double,
+        @field:DecimalMin(value = "0.00")
+        val costPrice: Double,
+        @field:DecimalMin(value = "0.01")
+        val salesPrice: Double,
         @field:DecimalMin(value = "0.00")
         val left: Double,
-        @field:DecimalMin(value = "0.01")
-        val price: Double,
+        val date: Date = Date(),
         @field:NotNull
-        val location: Point = Point(0.0, 0.0)
-) {
-    @Id
-    val id: String = UUID.randomUUID().toString()
-    val date: Date = Date()
-}
+        val location: Point = Point(0.0, 0.0))
