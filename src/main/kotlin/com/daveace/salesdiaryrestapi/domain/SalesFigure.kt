78ae233f97ca1fe89db09f562b3cd191d82f3801
richard <richardekong290@gmail.com>
2020-06-09 12:39:35 +0100

    create class SalesFigure.kt

---
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/domain/SalesFigure.kt
@@ -0,0 +1,28 @@
+package com.daveace.salesdiaryrestapi.domain
+
+import com.fasterxml.jackson.annotation.JsonIgnore
+
+data class SalesFigure(@JsonIgnore val events: MutableList<SalesEvent>) {
+
+    val totalCost: Double = getTotalCost(events)
+    val totalSales: Double = getTotalSales(events)
+    val totalProfit: Double = getTotalProfit(getProfits(events))
+
+    private fun getTotalCost(events: MutableList<SalesEvent>): Double {
+        return events.asSequence().sumByDouble { it.costPrice * it.quantitySold }
+    }
+
+    private fun getTotalSales(events: MutableList<SalesEvent>): Double {
+        return events.asSequence().sumByDouble { it.quantitySold }
+    }
+
+    private fun getProfits(events: MutableList<SalesEvent>): MutableList<Double> {
+        return events.asSequence().map{ getProfit(it) }.toMutableList()
+    }
+
+    private fun getTotalProfit(profits: MutableList<Double>): Double {
+        return profits.asSequence().sumByDouble { it }
+    }
+
+    private fun getProfit(event: SalesEvent) = event.salesPrice - event.costPrice
+}
