5c07c4e921af8fc7630491d8ad5f456bc4c39607
richard <richardekong290@gmail.com>
2020-06-10 22:03:58 +0100

    add function declaration to this interface

--- src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveSalesEventService.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/service/ReactiveSalesEventService.kt
@@ -1,4 +1,33 @@
 package com.daveace.salesdiaryrestapi.service
 
+import com.daveace.salesdiaryrestapi.domain.SalesEvent
+import com.daveace.salesdiaryrestapi.domain.SalesMetrics
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.util.*
+
 interface ReactiveSalesEventService {
+
+    fun saveSalesEvent(salesEvent: SalesEvent): Mono<SalesEvent>
+    fun findSalesEvent(id: String): Mono<SalesEvent>
+    fun findSalesEvents(): Flux<SalesEvent>
+    fun findSalesEvents(date: LocalDate): Flux<SalesEvent>
+    fun findDailySalesEvents(): Flux<SalesEvent>
+    fun findWeeklySalesEvents(): Flux<SalesEvent>
+    fun findMonthlySalesEvents(): Flux<SalesEvent>
+    fun findQuarterlySalesEvents(): Flux<SalesEvent>
+    fun findSemesterSalesEvents(): Flux<SalesEvent>
+    fun findYearlySalesEvents(): Flux<SalesEvent>
+    fun findSalesEvents(from: LocalDate, to: LocalDate): Flux<SalesEvent>
+    fun findSalesEventsMetrics(date: LocalDate): Flux<SalesMetrics>
+    fun findDailySalesEventsMetrics(): Flux<SalesMetrics>
+    fun findWeeklySalesEventsMetrics(): Flux<SalesMetrics>
+    fun findMonthlySalesEventsMetrics(): Flux<SalesMetrics>
+    fun findQuarterlySalesEventsMetrics(): Flux<SalesMetrics>
+    fun findSemesterSalesEventsMetrics(): Flux<SalesMetrics>
+    fun findYearlySalesEventsMetrics(): Flux<SalesMetrics>
+    fun findSalesEventsMetrics(from: LocalDate, to: LocalDate): Flux<SalesMetrics>
+
 }
