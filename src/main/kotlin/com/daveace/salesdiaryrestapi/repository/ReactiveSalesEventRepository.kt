ddb3610f2aff5e3b0b428de50fbaa34dc2005951
richard <richardekong290@gmail.com>
2020-06-10 22:02:40 +0100

    add convinient function for querying sales events by date

--- src/main/kotlin/com/daveace/salesdiaryrestapi/repository/ReactiveSalesEventRepository.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/repository/ReactiveSalesEventRepository.kt
@@ -2,6 +2,9 @@ package com.daveace.salesdiaryrestapi.repository
 
 import com.daveace.salesdiaryrestapi.domain.SalesEvent
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Flux
+import java.time.LocalDate
 
 interface ReactiveSalesEventRepository: ReactiveMongoRepository<SalesEvent, String> {
+    fun findSalesEventsByDate(date: LocalDate): Flux<SalesEvent>
 }
