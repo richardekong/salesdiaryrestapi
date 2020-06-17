08c8cc60d347479f1dc1e8b0dd40db56feea58ae
richard <richardekong290@gmail.com>
2020-06-17 22:36:27 +0100

    refactor declaring class as a normal kotlin class, expanding its body with properties and secondary constructor

--- src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/SalesMetricsModel.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/hateoas/model/SalesMetricsModel.kt
@@ -3,4 +3,18 @@ package com.daveace.salesdiaryrestapi.hateoas.model
 import com.daveace.salesdiaryrestapi.domain.SalesMetrics
 import org.springframework.hateoas.RepresentationModel
 
-data class SalesMetricsModel(val metrics: SalesMetrics): RepresentationModel<SalesMetricsModel>()
+class SalesMetricsModel(): RepresentationModel<SalesMetricsModel>(){
+
+    lateinit var category: String
+    lateinit var totalCost:Number
+    lateinit var totalSales:Number
+    lateinit var totalProfit:Number
+
+    constructor(metrics: SalesMetrics):this(){
+        this.category = metrics.category
+        this.totalCost = metrics.totalCost
+        this.totalSales = metrics.totalSales
+        this.totalProfit = metrics.totalProfit
+    }
+
+}
