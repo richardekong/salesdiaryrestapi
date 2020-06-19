f97c00fcb489cfd3c1146e51e02d41eeda0d12ba
richard <richardekong290@gmail.com>
2020-06-19 21:31:20 +0100

    refactor this class

--- src/main/kotlin/com/daveace/salesdiaryrestapi/authentication/AuthenticatedUser.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/authentication/AuthenticatedUser.kt
@@ -2,27 +2,18 @@ package com.daveace.salesdiaryrestapi.authentication
 
 import com.daveace.salesdiaryrestapi.domain.User
 import com.daveace.salesdiaryrestapi.exceptionhandling.AuthenticationException
-import com.daveace.salesdiaryrestapi.repository.ReactiveUserRepository
 import com.daveace.salesdiaryrestapi.service.ReactiveUserService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
-import java.lang.RuntimeException
 
 @Component
 class AuthenticatedUser {
 
-    private lateinit var userRepository: ReactiveUserRepository
-
     @Autowired
     private lateinit var userService: ReactiveUserService
-
-    @Autowired
-    fun setUserRepository(userRepository: ReactiveUserRepository) {
-        this.userRepository = userRepository
-    }
 
     fun ownsThisAccount(email: String): Mono<User> {
         return getCurrentUser()
