46e7a4f755003dc7cae6528af3240f8027bc8b08
richard <richardekong290@gmail.com>
2020-06-30 17:03:22 +0100

    modified request handlers by determining if current user is authorized to make them

--- src/main/kotlin/com/daveace/salesdiaryrestapi/controller/TraderController.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/controller/TraderController.kt
@@ -1,8 +1,5 @@
 package com.daveace.salesdiaryrestapi.controller
 
-import com.daveace.salesdiaryrestapi.authentication.AuthenticatedUser
-import com.daveace.salesdiaryrestapi.authentication.TokenUtil
-import com.daveace.salesdiaryrestapi.configuration.SortConfigurationProperties
 import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.API
 import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.SALES_DIARY_TRADER
 import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.SALES_DIARY_TRADERS
@@ -15,7 +12,6 @@ import com.daveace.salesdiaryrestapi.hateoas.link.ReactiveLinkSupport
 import com.daveace.salesdiaryrestapi.hateoas.model.CustomerModel
 import com.daveace.salesdiaryrestapi.hateoas.model.ProductModel
 import com.daveace.salesdiaryrestapi.hateoas.model.TraderModel
-import com.daveace.salesdiaryrestapi.page.Paginator
 import com.daveace.salesdiaryrestapi.service.ReactiveTraderService
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.data.domain.PageRequest
@@ -30,43 +26,24 @@ import javax.validation.Valid
 
 @RestController
 @RequestMapping(API)
-class TraderController() : ReactiveLinkSupport {
+class TraderController() : BaseController(), ReactiveLinkSupport {
 
     private lateinit var traderService: ReactiveTraderService
-    private lateinit var paginator: Paginator
-    private lateinit var sortProperties: SortConfigurationProperties
-    private lateinit var tokenUtil: TokenUtil
-    private lateinit var authenticatedUser: AuthenticatedUser
-
-    companion object {
-        const val DEFAULT_SIZE = "1"
-        const val DEFAULT_PAGE = "0"
-        const val DEFAULT_SORT_ORDER = "asc"
-        const val DEFAULT_SORT_FIELD = "id"
-    }
 
     @Autowired
-    constructor(
-            traderService: ReactiveTraderService,
-            paginator: Paginator,
-            sortProperties: SortConfigurationProperties,
-            tokenUtil: TokenUtil,
-            authenticatedUser: AuthenticatedUser) : this() {
-
+    constructor(traderService: ReactiveTraderService) : this() {
         this.traderService = traderService
-        this.paginator = paginator
-        this.sortProperties = sortProperties
-        this.tokenUtil = tokenUtil
-        this.authenticatedUser = authenticatedUser
     }
 
     @GetMapping("$SALES_DIARY_TRADER{email}")
     fun findTrader(@PathVariable email: String): Mono<TraderModel> {
-        return authenticatedUser.ownsThisAccount(email)
+        return authenticatedUser
+                .ownsThisAccount(email)
                 .flatMap { traderService.findTrader(email) }
                 .flatMap { trader ->
                     respondWithReactiveLink(TraderModel(trader),
-                            methodOn(this::class.java).findTrader(email))
+                            methodOn(this::class.java)
+                                    .findTrader(email))
                 }
     }
 
@@ -77,7 +54,6 @@ class TraderController() : ReactiveLinkSupport {
             @RequestParam(name = "sort", defaultValue = DEFAULT_SORT_FIELD) by: String,
             @RequestParam(name = "dir", defaultValue = DEFAULT_SORT_ORDER) dir: String
     ): Flux<PagedModel<TraderModel>> {
-
         return linkTo(methodOn(this::class.java)
                 .findAllTraders(size, page, by, dir))
                 .withSelfRel()
@@ -92,24 +68,44 @@ class TraderController() : ReactiveLinkSupport {
 
     @PostMapping("$SALES_DIARY_TRADER{email}/products")
     fun addProduct(@PathVariable email: String, @RequestBody @Valid product: Product): Mono<ProductModel> {
-
-        return traderService.addProduct(email, product).flatMap {
-            respondWithReactiveLink(ProductModel(it), methodOn(this.javaClass).addProduct(email, product))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedByEmail(email)
+                .flatMap {
+                    traderService.addProduct(email, product)
+                            .flatMap {
+                                respondWithReactiveLink(ProductModel(it),
+                                        methodOn(this.javaClass)
+                                                .addProduct(email, product))
+                            }
+                }
     }
 
     @PostMapping("$SALES_DIARY_TRADER{email}/customers")
     fun addCustomer(@PathVariable email: String, @RequestBody @Valid customer: Customer): Mono<CustomerModel> {
-        return traderService.addCustomer(email, customer).flatMap {
-            respondWithReactiveLink(CustomerModel(it), methodOn(this.javaClass).addCustomer(email, customer))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedByEmail(email)
+                .flatMap {
+                    traderService.addCustomer(email, customer)
+                            .flatMap { addedCustomer ->
+                                respondWithReactiveLink(CustomerModel(addedCustomer),
+                                        methodOn(this.javaClass)
+                                                .addCustomer(email, customer))
+                            }
+                }
     }
 
     @GetMapping("$SALES_DIARY_TRADER{traderId}/products/{productId}")
     fun findTraderProduct(@PathVariable traderId: String, @PathVariable productId: String): Mono<ProductModel> {
-        return traderService.findTraderProduct(traderId, productId).flatMap {
-            respondWithReactiveLink(ProductModel(it), methodOn(this.javaClass).findTraderProduct(traderId, productId))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(traderId)
+                .flatMap {
+                    traderService.findTraderProduct(traderId, productId)
+                            .flatMap { product ->
+                                respondWithReactiveLink(ProductModel(product),
+                                        methodOn(this.javaClass)
+                                                .findTraderProduct(traderId, productId))
+                            }
+                }
     }
 
     @GetMapping("$SALES_DIARY_TRADER{traderId}/products")
@@ -120,21 +116,36 @@ class TraderController() : ReactiveLinkSupport {
             @RequestParam(name = "dir", defaultValue = DEFAULT_SORT_ORDER) dir: String,
             @PathVariable traderId: String): Flux<PagedModel<ProductModel>> {
 
-        return linkTo(methodOn(this.javaClass).findTraderProducts(
-                size, page, by, dir, traderId
-        )).withSelfRel().toMono().flatMapMany { link ->
-            paginator.paginate(ProductModelAssembler(),
-                    traderService.findTraderProducts(traderId),
-                    PageRequest.of(page, size), link,
-                    configureSortProperties(by, dir))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(traderId)
+                .flatMapMany {
+                    linkTo(methodOn(this.javaClass)
+                            .findTraderProducts(size, page, by, dir, traderId))
+                            .withSelfRel()
+                            .toMono()
+                            .flatMapMany { link ->
+                                paginator.paginate(ProductModelAssembler(),
+                                        traderService.findTraderProducts(traderId),
+                                        PageRequest.of(page, size), link,
+                                        configureSortProperties(by, dir))
+                            }
+                }
+
     }
 
     @GetMapping("$SALES_DIARY_TRADER{id}/customers/{email}")
     fun findTraderCustomer(@PathVariable id: String, @PathVariable email: String): Mono<CustomerModel> {
-        return traderService.findTraderCustomer(id, email).flatMap {
-            respondWithReactiveLink(CustomerModel(it), methodOn(this.javaClass).findTraderCustomer(id, email))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(id)
+                .flatMap {
+                    traderService.findTraderCustomer(id, email)
+                            .flatMap {
+                                respondWithReactiveLink(CustomerModel(it),
+                                        methodOn(this.javaClass)
+                                                .findTraderCustomer(id, email))
+                            }
+                }
+
     }
 
     @GetMapping("$SALES_DIARY_TRADER{id}/customers")
@@ -145,54 +156,77 @@ class TraderController() : ReactiveLinkSupport {
             @RequestParam(name = "dir", defaultValue = DEFAULT_SORT_ORDER) dir: String,
             @PathVariable id: String): Flux<PagedModel<CustomerModel>> {
 
-        return linkTo(methodOn(this.javaClass).findTraderCustomers(
-                size, page, by, dir, id)).withSelfRel().toMono().flatMapMany { link ->
-            paginator.paginate(CustomerModelAssembler(),
-                    traderService.findTraderCustomers(id),
-                    PageRequest.of(page, size), link,
-                    configureSortProperties(by, dir))
-        }
+        return authenticatedUser.isCurrentUserAuthorizedById(id)
+                .flatMapMany {
+                    linkTo(methodOn(this.javaClass)
+                            .findTraderCustomers(size, page, by, dir, id))
+                            .withSelfRel()
+                            .toMono()
+                            .flatMapMany { link ->
+                                paginator.paginate(CustomerModelAssembler(),
+                                        traderService.findTraderCustomers(id),
+                                        PageRequest.of(page, size), link,
+                                        configureSortProperties(by, dir))
+                            }
+                }
     }
 
     @PatchMapping(SALES_DIARY_TRADERS)
     fun <V> updateTrader(@RequestBody trader: MutableMap<String, V>): Mono<TraderModel> {
-        return traderService.updateTrader(trader).flatMap {
-            respondWithReactiveLink(TraderModel(it), methodOn(this.javaClass).updateTrader(trader))
-        }
+        return authenticatedUser
+                .getCurrentUser()
+                .flatMap { currentUser ->
+                    traderService.updateTrader(currentUser.id, trader)
+                            .flatMap { updatedTrader ->
+                                respondWithReactiveLink(TraderModel(updatedTrader),
+                                        methodOn(this.javaClass)
+                                                .updateTrader(trader))
+                            }
+                }
     }
 
     @PatchMapping("$SALES_DIARY_TRADER{id}/customers/{cEmail}")
     fun <V> updateTraderCustomer(@PathVariable id: String, @PathVariable cEmail: String, @RequestBody customer: MutableMap<String, V>): Mono<CustomerModel> {
-        return traderService.updateTraderCustomer(id, customer, cEmail).flatMap {
-            respondWithReactiveLink(CustomerModel(it), methodOn(this.javaClass)
-                    .updateTraderCustomer(id, cEmail, customer))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(id)
+                .flatMap { traderService.findTraderCustomer(id, cEmail) }
+                .flatMap {
+                    traderService.updateTraderCustomer(id, customer, cEmail)
+                            .flatMap {
+                                respondWithReactiveLink(CustomerModel(it), methodOn(this.javaClass)
+                                        .updateTraderCustomer(id, cEmail, customer))
+                            }
+                }
     }
 
     @PatchMapping("$SALES_DIARY_TRADER{tId}/products/{pId}")
     fun <V> updateTraderProduct(@PathVariable tId: String, @PathVariable pId: String, @RequestBody product: MutableMap<String, V>): Mono<ProductModel> {
-        return traderService.updateTraderProduct(tId, product, pId).flatMap {
-            respondWithReactiveLink(ProductModel(it), methodOn(this.javaClass)
-                    .updateTraderProduct(tId, pId, product))
-        }
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(tId)
+                .flatMap { traderService.findTraderProduct(tId, pId) }
+                .flatMap {
+                    traderService.updateTraderProduct(tId, product, pId)
+                            .flatMap {
+                                respondWithReactiveLink(ProductModel(it), methodOn(this.javaClass)
+                                        .updateTraderProduct(tId, pId, product))
+                            }
+                }
     }
 
     @DeleteMapping("$SALES_DIARY_TRADER{tId}/products/{pId}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     fun deleteTraderProduct(@PathVariable tId: String, @PathVariable pId: String): Mono<Void> {
-        return traderService.deleteTraderProduct(tId, pId)
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(tId)
+                .flatMap { traderService.deleteTraderProduct(tId, pId) }
     }
 
     @DeleteMapping("$SALES_DIARY_TRADER{tId}/customers/{cEmail}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     fun deleteTraderCustomer(@PathVariable tId: String, @PathVariable cEmail: String): Mono<Void> {
-        return traderService.deleteTraderCustomer(tId, cEmail)
-    }
-
-    private fun configureSortProperties(by: String, dir: String): SortConfigurationProperties {
-        sortProperties.by = by
-        sortProperties.dir = dir
-        return sortProperties
+        return authenticatedUser
+                .isCurrentUserAuthorizedById(tId)
+                .flatMap { traderService.deleteTraderCustomer(tId, cEmail) }
     }
 
 }
