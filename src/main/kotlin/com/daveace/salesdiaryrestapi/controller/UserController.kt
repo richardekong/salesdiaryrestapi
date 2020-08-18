a67484e9e71e8fc1b2243896b7f2efdcec72f927
richard <richardekong290@gmail.com>
2020-08-18 14:23:04 +0100

    add functionalities for notifying actions concerning user account creation, update and deletion through emails marked up by html

--- src/main/kotlin/com/daveace/salesdiaryrestapi/controller/UserController.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/controller/UserController.kt
@@ -7,6 +7,7 @@ import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.SALES_D
 import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.SALES_DIARY_AUTH_SIGN_UP_USERS
 import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.SALES_DIARY_USER
 import com.daveace.salesdiaryrestapi.controller.ControllerPath.Companion.SALES_DIARY_USERS
+import com.daveace.salesdiaryrestapi.domain.Mail
 import com.daveace.salesdiaryrestapi.domain.PASSWORD_SIZE_VAL_MSG
 import com.daveace.salesdiaryrestapi.domain.User
 import com.daveace.salesdiaryrestapi.exceptionhandling.AuthenticationException
@@ -25,8 +26,11 @@ import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.linkTo
 import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder.methodOn
 import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.*
+import org.springframework.web.server.ServerWebExchange
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.validation.Valid
 import javax.validation.constraints.Size
 
@@ -43,6 +47,11 @@ class UserController() : BaseController() {
         const val DEFAULT_PAGE = "0"
         const val DEFAULT_SORT_FIELD = "id"
         const val DEFAULT_SORT_ORDER = "asc"
+        const val SIGN_UP_MAIL_TEMPLATE = "sign_up_mail_template"
+        const val PASSWORD_RESET_LINK_MAIL_TEMPLATE = "password_reset_link_mail_template"
+        const val PASSWORD_RESET_MAIL_TEMPLATE = "password_reset_mail_template"
+        const val ACCOUNT_DELETION_MAIL_TEMPLATE = "account_deletion_mail_template"
+        const val LOG_IN_MAIL_TEMPLATE = "log_in_mail_template"
     }
 
     @Autowired
@@ -53,20 +62,28 @@ class UserController() : BaseController() {
 
     @PostMapping(SALES_DIARY_AUTH_SIGN_UP_USERS, produces = ["application/json"])
     @ResponseStatus(value = HttpStatus.CREATED)
-    fun signUpUser(@RequestBody @Valid user: User): Mono<UserModel> {
+    fun signUpUser(@RequestBody @Valid user: User, exchange: ServerWebExchange): Mono<UserModel> {
         val hash = encoderService.encode(user.userPassword)
         user.userPassword = hash
         return userService.create(user)
-                .flatMap {
+                .doOnSuccess {
+                    val subject = "Sales Diary Sign up"
+                    prepareAndSendEmailWithHTMLTemplate(
+                            SIGN_UP_MAIL_TEMPLATE,
+                            Mail(it.email, subject),
+                            it.toMap(),
+                            exchange
+                    )
+                }.flatMap {
                     respondWithReactiveLink(
                             UserModel(it),
-                            methodOn(this.javaClass).signUpUser(it)
+                            methodOn(this.javaClass).signUpUser(it, exchange)
                     )
                 }
     }
 
     @PostMapping(SALES_DIARY_AUTH_LOGIN_USERS)
-    fun logInUser(@RequestBody credentials: Map<String, String>): Mono<TokenModel> {
+    fun logInUser(@RequestBody credentials: Map<String, String>, exchange: ServerWebExchange): Mono<TokenModel> {
         val email: String? = credentials["email"]
         val password: String? = credentials["password"]
         return userService.findUserByEmail(email!!)
@@ -74,16 +91,35 @@ class UserController() : BaseController() {
                     throw AuthenticationException(
                             HttpStatus.UNAUTHORIZED.reasonPhrase)
                 })
-                .map {
-                    if (!encoderService.matches(password, it.password))
-                        throw RuntimeException(WRONG_CREDENTIAL)
-                    tokenUtil.generateToken(it)
+                .filter { encoderService.matches(password, it.password) }
+                .switchIfEmpty(Mono.fromRunnable {
+                    throw RuntimeException(WRONG_CREDENTIAL)
+                })
+                .map { tokenUtil.generateToken(it) }
+                .doOnSuccess {
+                    val subject = "Log In Confirmation"
+                    val accessTimeKey = "access_time"
+                    val userKey = "user"
+                    val resetLinkKey = "reset_link"
+                    val currentUser: User = tokenUtil.getUserFromToken(it)
+                    val resetLink: String = linkTo(methodOn(this.javaClass)
+                            .resetPassword(it, "\u0020", exchange))
+                            .withSelfRel().toMono().toFuture().join().href
+
+                    val recipientData: MutableMap<String, Any?> = mutableMapOf(
+                            accessTimeKey to LocalDateTime.now().format(DateTimeFormatter
+                                    .ofPattern(DATE_TIME_PATTERN)),
+                            resetLinkKey to resetLink,
+                            userKey to currentUser)
+                    prepareAndSendEmailWithHTMLTemplate(
+                            LOG_IN_MAIL_TEMPLATE, Mail(email, subject),
+                            recipientData, exchange)
                 }
                 .flatMap {
                     respondWithReactiveLink(
                             TokenModel(it),
                             methodOn(this::class.java)
-                                    .logInUser(credentials)
+                                    .logInUser(credentials, exchange)
                     )
                 }
     }
@@ -127,17 +163,28 @@ class UserController() : BaseController() {
     }
 
     @GetMapping("$SALES_DIARY_AUTH_PASSWORD_RESET_LINK{email}")
-    fun requestPasswordResetLink(@PathVariable email: String): Mono<String> {
+    fun requestPasswordResetLink(@PathVariable email: String, exchange: ServerWebExchange): Mono<String> {
         return userService.findUserByEmail(email)
                 .switchIfEmpty(Mono.fromRunnable {
                     throw RestException("Wrong email address!")
-                })
-                .flatMap {
+                }).flatMap {
                     val token: String = tokenUtil.generateToken(it, validity = 60000L)
-                    val monoLink: Mono<Link> = linkTo(methodOn(
-                            this.javaClass).resetPassword(token, ""))
-                            .withSelfRel().toMono()
-                    userService.sendPasswordResetLink(email, monoLink)
+                    linkTo(methodOn(this.javaClass)
+                            .resetPassword(token, "\u0020", exchange))
+                            .withSelfRel()
+                            .toMono()
+                            .flatMap { passwordResetLink ->
+                                val subject = "Password Reset Link"
+                                val linkKey = "link"
+                                val userKey = "user"
+                                val recipientData: MutableMap<String, Any?> = mutableMapOf(
+                                        linkKey to passwordResetLink.href,
+                                        userKey to it)
+                                prepareAndSendEmailWithHTMLTemplate(
+                                        PASSWORD_RESET_LINK_MAIL_TEMPLATE,
+                                        Mail(it.email, subject),
+                                        recipientData, exchange)
+                            }
                 }
     }
 
@@ -146,14 +193,21 @@ class UserController() : BaseController() {
             @PathVariable token: String,
             @RequestParam(name = "password")
             @Size(message = PASSWORD_SIZE_VAL_MSG)
-            password: String): Mono<UserModel> {
+            password: String, exchange: ServerWebExchange): Mono<UserModel> {
 
         return userService.resetUserPassword(token, password)
+                .doOnSuccess {
+                    val subject = "Password Reset Operation"
+                    prepareAndSendEmailWithHTMLTemplate(
+                            PASSWORD_RESET_MAIL_TEMPLATE,
+                            Mail(it.email, subject),
+                            it.toMap(), exchange)
+                }
                 .flatMap { user ->
                     respondWithReactiveLink(
                             UserModel(user),
                             methodOn(this.javaClass)
-                                    .resetPassword(token, password))
+                                    .resetPassword(token, user.password, exchange))
                 }
                 .doOnSuccess {
                     InMemoryTokenStore.revokeToken(it.email)
@@ -163,14 +217,20 @@ class UserController() : BaseController() {
 
     @DeleteMapping("$SALES_DIARY_USER{email}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    fun deleteUser(@PathVariable email: String): Mono<Void> {
+    fun deleteUser(@PathVariable email: String, exchange: ServerWebExchange): Mono<Void> {
         return authenticatedUser.isCurrentUserAuthorizedByEmail(email)
                 .flatMap {
                     userService.deleteUserByEmail(email)
-                            .doOnSuccess { InMemoryTokenStore.revokeToken(email) }
+                            .doOnSuccess {
+                                val subject = "Sales Diary Account deletion"
+                                prepareAndSendEmailWithHTMLTemplate(
+                                        ACCOUNT_DELETION_MAIL_TEMPLATE,
+                                        Mail(email, subject),
+                                        mutableMapOf(),
+                                        exchange)
+                                InMemoryTokenStore.revokeToken(email)
+                            }
                 }
-
-
     }
 
 }
