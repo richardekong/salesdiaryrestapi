49379bfb60c7799596e7f6657206e8934ea645d6
richard <richardekong290@gmail.com>
2020-11-15 18:01:21 +0100

    rename this file

--- src/main/kotlin/com/daveace/salesdiaryrestapi/authentication/AuthenticatedUser.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/authentication/AuthenticatedUser.kt
@@ -3,17 +3,23 @@ package com.daveace.salesdiaryrestapi.authentication
 import com.daveace.salesdiaryrestapi.domain.User
 import com.daveace.salesdiaryrestapi.exceptionhandling.AuthenticationException
 import com.daveace.salesdiaryrestapi.service.ReactiveUserService
+import org.jetbrains.annotations.NotNull
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
+import java.security.Principal
 
 @Component
 class AuthenticatedUser {
 
-    @Autowired
     private lateinit var userService: ReactiveUserService
+
+    @Autowired
+    fun initUserService(userService: ReactiveUserService) {
+        this.userService = userService
+    }
 
     fun ownsThisAccount(email: String): Mono<User> {
         return getCurrentUser()
@@ -24,10 +30,18 @@ class AuthenticatedUser {
                 })
     }
 
+    fun ownsThisAccount(email: String, principal: Principal):Mono<User> = getCurrentUser(principal)
+            .filter { it.email == email }
+            .switchIfEmpty(Mono.fromRunnable {
+                throw AuthenticationException(
+                        HttpStatus.UNAUTHORIZED.reasonPhrase
+                )
+            })
+
     fun isCurrentUserAuthorizedByEmail(email: String): Mono<Boolean> {
         return getCurrentUser()
                 .filter { it.email == email }
-                .switchIfEmpty(Mono.fromRunnable{
+                .switchIfEmpty(Mono.fromRunnable {
                     throw AuthenticationException(
                             HttpStatus.UNAUTHORIZED.reasonPhrase
                     )
@@ -35,11 +49,20 @@ class AuthenticatedUser {
                 .map { it != null }
 
     }
+
+    fun isCurrentUserAuthorizedByEmail(email: String, principal: Principal): Mono<Boolean> = getCurrentUser(principal)
+            .filter { it.email == email }
+            .switchIfEmpty(Mono.fromRunnable {
+                throw AuthenticationException(
+                        HttpStatus.UNAUTHORIZED.reasonPhrase
+                )
+            })
+            .map { it != null }
 
     fun isCurrentUserAuthorizedById(id: String): Mono<Boolean> {
         return getCurrentUser()
                 .filter { it.id == id }
-                .switchIfEmpty(Mono.fromRunnable{
+                .switchIfEmpty(Mono.fromRunnable {
                     throw AuthenticationException(
                             HttpStatus.UNAUTHORIZED.reasonPhrase
                     )
@@ -47,9 +70,21 @@ class AuthenticatedUser {
                 .map { it != null }
     }
 
+    fun isCurrentUserAuthorizedById(id: String, principal: Principal): Mono<Boolean> = getCurrentUser(principal)
+            .filter { it.id == id }
+            .switchIfEmpty(Mono.fromRunnable {
+                throw AuthenticationException(
+                        HttpStatus.UNAUTHORIZED.reasonPhrase
+                )
+            })
+            .map { it != null }
+
     fun ownsThisAccountById(id: String): Mono<Boolean> {
         return getCurrentUser().filter { it.id == id }.map { it != null }
     }
+
+    fun ownsThisAccountById(id: String, principal: Principal): Mono<Boolean> = getCurrentUser(principal)
+            .filter { it.id == id }.map { it != null }
 
     fun getCurrentUser(): Mono<User> = Mono.just(
             SecurityContextHolder
@@ -59,5 +94,9 @@ class AuthenticatedUser {
             .flatMap {
                 userService.findUserByEmail(it)
             }
+
+    fun getCurrentUser(@NotNull principal: Principal): Mono<User> = Mono.just(
+            principal.name)
+            .flatMap { userService.findUserByEmail(it) }
 
 }
--- src/main/kotlin/com/daveace/salesdiaryrestapi/exceptionhandling/RestException.kt
+++
@@ -1,3 +0,0 @@
-package com.daveace.salesdiaryrestapi.exceptionhandling
-
-class RestException(override val message:String) : RuntimeException(message)
