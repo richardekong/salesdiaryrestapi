2522034056a86dd9f3f6e72ae7116235ad7076bb
richard <richardekong290@gmail.com>
2020-12-09 15:03:11 +0100

    modify toMap() function

--- src/main/kotlin/com/daveace/salesdiaryrestapi/mapper/Mappable.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/mapper/Mappable.kt
@@ -1,15 +1,20 @@
 package com.daveace.salesdiaryrestapi.mapper
 
-import com.fasterxml.jackson.databind.ObjectMapper
+import kotlin.reflect.full.memberProperties
+import kotlin.reflect.jvm.isAccessible
 
 interface Mappable {
 
     fun toMap(): MutableMap<String, Any?> {
-        return ObjectMapper().convertValue(this, getMapInstance()::class.java)
+        val map = mutableMapOf<String, Any?>()
+        this::class.memberProperties.parallelStream()
+                .forEach {
+                    it.isAccessible = true
+                    map[it.name] = it.getter.call(this)
+                    it.isAccessible = false
+                }
+        return map
     }
-
-    private fun getMapInstance(): MutableMap<String, Any?> = mutableMapOf()
-
 
 }
 
