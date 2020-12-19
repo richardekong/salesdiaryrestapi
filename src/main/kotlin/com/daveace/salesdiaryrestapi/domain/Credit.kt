293767f4821034713056907106ec46e69bb11a9c
richard <richardekong290@gmail.com>
2020-12-19 22:35:35 +0100

    create credit domain class

--- src/main/kotlin/com/daveace/salesdiaryrestapi/domain/Credit.kt
+++ src/main/kotlin/com/daveace/salesdiaryrestapi/domain/Credit.kt
@@ -8,21 +8,30 @@ import java.time.LocalDate
 @Document
 data class Credit(@field:Id val id: String = SalesDiaryId.generateId()) : Mappable {
 
-    private var traderId:String = ""
+    private var traderId: String = ""
     private var customerId: String = ""
     private var productId: String = ""
+    private var product: String = ""
+    private var costPrice: Double = 0.0
     private var salesPrice: Double = 0.0
     private var quantity: Double = 0.0
+    private var left: Double = 0.0
     private var date: LocalDate = LocalDate.now()
+    private var location = MutableList(2) { it * 0.0 }
     private var redeemed: Boolean = false
 
     constructor(event: SalesEvent) : this() {
         traderId = event.traderId
         customerId = event.customerId
         productId = event.productId
-        salesPrice = event.salesPrice
+        product = event.product
+        costPrice = event.costPrice
+        salesPrice = event.salesPrice.times(-1)
         quantity = event.quantitySold
+        left = event.left
         date = event.date
+        location[0] = event.location[0]
+        location[1] = event.location[1]
     }
 
     fun redeem(value: Boolean) {
@@ -32,9 +41,13 @@ data class Credit(@field:Id val id: String = SalesDiaryId.generateId()) : Mappab
     fun traderId() = traderId
     fun customerId() = customerId
     fun productId() = productId
+    fun product() = product
+    fun costPrice() = costPrice
     fun salesPrice() = salesPrice
     fun quantity() = quantity
+    fun left() = left
     fun date() = date
+    fun location() = location
     fun redeemed() = redeemed
 
 
